48e18eca865c17119596a665f4f9c970ef5a9a64
atharvatiwari22882828 <atharvatiwari22882828@gmail.com>
2020-11-07 20:52:24 +0530

    first

---
+++ Untitled2.c
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int  a,b;
+
+cout <<"enter the frist number:";
+
+cin >> a;
+
+cout <<"enter the second number:";
+
+cin >> b;
+
+cout <<"sum of a and b is "<< a+b;
+
+return 0;
+}
---
+++ gym management.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+int a,b;
+
+cout <<"enter the frist number:";
+cin >>a;
+
+cout <<"enter the second number:";
+cin >>b;
+
+cout <<a+b;
+
+cout <<"\n";
+
+cout <<a-b;
+
+cout <<"\n";
+
+cout <<a*b;
+
+cout <<"\n";
+
+cout <<a/b;
+
+return 0;
+
+
+
+}
+
+
